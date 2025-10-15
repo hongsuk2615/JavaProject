6099ce0bc4625a2d1b775a1aaa5b8fc7eef48d64
김홍석 <hongsuk2615@gmail.com>
2025-10-15 17:37:14 +0900

    미사용 구문 삭제

--- src/main/java/com/springboot/springproject/auth/controller/AuthController.java
+++ src/main/java/com/springboot/springproject/auth/controller/AuthController.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AuthController {
 
-    private final JwtProvider jwtProvider;
-
-    public AuthController(JwtProvider jwtProvider) {
-        this.jwtProvider = jwtProvider;
-    }
-
     @GetMapping("/auth/login")
     public String loginPage() {
         return "auth/login";
