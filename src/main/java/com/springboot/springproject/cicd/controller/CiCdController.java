62b030806e680634b17d3ecc6082ca28cae9a86d
김홍석 <hongsuk2615@gmail.com>
2025-10-24 12:52:17 +0900

    뷰단 리팩토링:재사용 레이아웃처리

---
+++ src/main/java/com/springboot/springproject/cicd/controller/CiCdController.java
@@ -0,0 +1,12 @@
+package com.springboot.springproject.cicd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CiCdController {
+    @GetMapping("/cicd")
+    public String index(){
+        return "cicd/cicd";
+    }
+}
